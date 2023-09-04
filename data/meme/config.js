b9cc45b5f7bcae30b0c7e60416058766c04d24fb
jokereven <zjing125.cn@gmail.com>
2023-09-04 14:01:53 +0800

    chore: remove data

--- data/meme/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    /**
-     * @ch 起始区块高度，项目地址中最早期项目的起始区块即可，合理控制该值可提高计算速度
-     * @eh start block number of getting logs, please ensure this value contains
-     * all the minting logs of your projects
-     */
-    fromBlock: 14850000,
-    address: '0x9f82a438300627cd862f46c5a2831da342d570df'
-}
--- data/meme/meme.js
+++
@@ -1,7 +0,0 @@
-import { config } from "./config.js"
-import { findKing } from "./utils/utils.js"
-console.clear()
-
-findKing(
-  config.address
-)
